a350678e0432616cad5b9726385d7fed19d26bd8
RealPatrickGuertin <63614060+RealPatrickGuertin@users.noreply.github.com>
2020-11-08 15:36:57 -0500

    Added runtime

--- code/Open_Cv.py
+++ code/Open_Cv.py
@@ -1,10 +1,27 @@
 import cv2
 import os
 import numpy as np
+import time
 
-for filename in os.listdir("images"):
-    if filename.endswith(".jpg"):
-        print(filename)
-        img = cv2.imread("images/" + filename)
-        cv2.imshow("output", img)
-        cv2.waitKey(1)
+### Take in relative path ###
+
+#fileDir = input("input file Directory: ")
+fileDir = "images"
+
+### get start time of program run for runtime analysis ###
+start_time = time.time()
+
+### function for reading in files of specified path ###
+def FileRead(FileDir):
+    for filename in os.listdir(fileDir):
+        if filename.endswith(".jpg"):
+            print(filename)
+            img = cv2.imread(FileDir + "/" + filename)
+            cv2.imshow("output", img)
+            cv2.waitKey(10)
+
+### use read file function ###
+FileRead(fileDir)
+
+### Print out the calculated runtime ###
+print("--- %s seconds ---" % (time.time() - start_time))
