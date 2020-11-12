ccfa537b89d7b5e3e9ab766cb62f8ce9697eee4d
RealPatrickGuertin <63614060+RealPatrickGuertin@users.noreply.github.com>
2020-11-12 11:15:36 -0500

    x

--- code/plot_err.py
+++ code/plot_err.py
@@ -2,8 +2,8 @@ import main
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(1, 11,None, np.int32)
-y = np.zeros(10, np.double)
+x = np.arange(1, 6,None, np.int32)
+y = np.zeros(5, np.double)
 
 it = 0
 for i in main.errorPercent:
